ecdd9dcdb57fb36399f6ce66b8186f22cdf2e305
Piotr Zieliński <thepateto@gmail.com>
2017-05-02 00:30:54 +0200

    Appd moved to root folder and added Author model in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,27 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from trans import trans
+import string
 
 class Author(models.Model):
     name = models.CharField(
         max_length=50,
         unique=True,
     )
-    slug = models(
+    slug = models.SlugField(
         max_length=50,
         unique=True,
         blank=True,
     )
 
+    def clean(self):
+        if not self.name:
+            raise ValidationError('Name field is required')
+
     def save(self, *args, **kwargs):
-        print('ARGS:', args)
-        print('KWARGS:', kwargs)
+        self.full_clean()
+        if not self.slug:
+            self.slug = trans(self.name).replace(string.whitespace, '').lower()
         super(type(self), self).save(*args, **kwargs)
 
     def __eq__(self, other):

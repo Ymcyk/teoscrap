3023917394df8c3d0030ff738669991640f32a08
Piotr Zieliński <thepateto@gmail.com>
2017-05-02 20:22:43 +0200

    Better regex in string_to_words_list()

--- blog/utils.py
+++ blog/utils.py
@@ -3,7 +3,7 @@ import string
 import re
 
 def string_to_words_list(text):
-    return re.findall('\w+', text)
+    return re.findall(r"'\w+|\w+'\w+|\w+'|\w+", text)
 
 def slugify(text):
     return trans(text).replace(' ', '').lower()
